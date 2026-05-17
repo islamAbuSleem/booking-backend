7c5fe1ec1b7e8cd1c538557f0e8e8a677933b22b
islam <abdelrahmani@aecl.com>
2026-05-18 00:59:15 +0300

    we added the repository, user entity and one step before the service

--- src/main/java/com/booking/BookingApplication.java
+++ src/main/java/com/booking/BookingApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BookingApplication {
 
     public static void main(String[] args) {
-        System.out.println("Welcome to the Booking Application!");
+        SpringApplication.run(BookingApplication.class, args);
         // You can add code here to initialize your application, such as setting up a database connection,
         // starting a web server, or displaying a user interface.
     }
